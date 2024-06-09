7a640baf12d0974262233f45bb388efc7d542e5c
Mahenina <kylleandriamahenina@gmail.com>
2024-06-09 18:58:29 +0300

    triangle de pascal

---
+++ pascal.c
@@ -0,0 +1,30 @@
+﻿#include <stdlib.h>
+#include <stdio.h>
+
+int fact(int n)
+{
+	int i=1;
+	int f=1;
+	
+	while(i<=n)
+	{
+		f=f*i;
+		i++;
+	}	
+	return f;
+}
+
+int main()
+{
+	int i,j,n,c=1;
+	printf("entrer n:");
+		scanf("%d",&n);
+	for(i=0;i<=n;i++)
+	{
+		for(j=0;j<=i;j++)
+		{
+			c=fact(i)/(fact(i-j)*fact(j));
+			printf("%d",c);
+		}
+	printf("\n");}
+}
